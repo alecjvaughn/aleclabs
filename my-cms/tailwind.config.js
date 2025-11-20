618d09af4de51782689d47ea577676d0fe351797
Alec Vaughn <alecjvaughn+git@gmail.com>
2025-11-20 17:48:22 -0600

    add next.js frontend base

--- my-cms/tailwind.config.js
+++ my-cms/tailwind.config.js
@@ -1,10 +1,17 @@
 import fireCMSConfig from "@firecms/ui/tailwind.config.js";
+// not a TS file so below doesn't work
+// @ts-ignore
+// import type { Config } from "tailwindcss";
 
-export default {
-    presets: [fireCMSConfig],
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-      "./node_modules/@firecms/**/*.{js,ts,jsx,tsx}"
-    ],
+// const config: Config = {
+const config = {
+  presets: [fireCMSConfig],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/cms/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@firecms/**/*.{js,ts,jsx,tsx}"
+  ]
 };
+export default config;
