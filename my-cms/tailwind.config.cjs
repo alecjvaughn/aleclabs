0d0fed653b202ff2c2a1fab91e9b4a5f2bcf920e
Alec Vaughn <alecj.vaughn@gmail.com>
2026-01-10 21:47:00 -0600

    test with local build

--- my-cms/tailwind.config.cjs
+++ my-cms/tailwind.config.cjs
@@ -2,10 +2,20 @@ module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './public/**/*.html'
+    './public/**/*.html',
+    './node_modules/@firecms/ui/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        headers: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#0070F4',
+        secondary: '#FF5B79',
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
